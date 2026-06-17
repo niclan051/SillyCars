9d719b4fc52beef024200bd2a6453bcc50fc5243
cputnam-a11y <119390378+cputnam-a11y@users.noreply.github.com>
2026-06-17 05:37:18 -0500

    Misc Cleaup + Deploy workflow (#26)

--- build.sh
+++ build.sh
@@ -19,5 +19,5 @@ cp mod/build/libs/* build/mods
 
 cd build
 mkdir -p ../out
-packwiz modrinth export -o ../out/cutie_cars.mrpack
+packwiz modrinth export -o ../out/silly_cars.mrpack
 packwiz refresh --build
